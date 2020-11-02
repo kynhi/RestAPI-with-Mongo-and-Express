4ef9a8958217b452024f73683a467c059b89ee08
Nhi Luong <nhi.luong@mavs.uta.edu>
2020-11-02 16:23:52 -0600

    add Validator

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,15 @@
+const validationHandler = require("../validations/validationHandler")
 exports.index = (req,res) =>{
   throw new Error("Some Random Error")
   res.send({message: "hi"});
+}
+
+exports.store = (req,res,next) => {
+  try {
+    validationHandler(req)
+    res.send({message:"The Name is" + req.body.name});
+
+  }catch(err){
+    next(err)
+  }
 }
